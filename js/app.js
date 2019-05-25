624e4c6701a617ecd0e569c2ac215c0c0e4e84a9
kcuyan <31530076+kcuyan@users.noreply.github.com>
2019-05-25 14:05:36 -0600

    Se finaliza proyecto con correciones

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 particlesJS.load('particles-js', 'particles.json', function() {
     console.log('callback - particles.js config loaded');
-  });
+});
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
